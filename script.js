6ed287072b383e7206c91a0f10a7fb7ea7f2257b
ethan123pereira <pereiraethan003@gmail.com>
2026-03-15 02:45:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function displayTasks(){
+let list = document.getElementById("taskList");
+list.innerHTML = "";
+
+tasks.forEach((task,index)=>{
+let li = document.createElement("li");
+
+li.innerHTML = `
+${task}
+<button class="delete" onclick="deleteTask(${index})">X</button>
+`;
+
+list.appendChild(li);
+});
+}
+
+function addTask(){
+let input = document.getElementById("taskInput");
+let task = input.value;
+
+if(task === "") return;
+
+tasks.push(task);
+localStorage.setItem("tasks", JSON.stringify(tasks));
+
+input.value="";
+displayTasks();
+}
+
+function deleteTask(index){
+tasks.splice(index,1);
+localStorage.setItem("tasks", JSON.stringify(tasks));
+displayTasks();
+}
+
+displayTasks();
